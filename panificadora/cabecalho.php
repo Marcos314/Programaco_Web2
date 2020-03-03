481c56d5d6d51414accf4b286bd52f87ec420bc4
Marcos Wesley <marcoswesley314@gmail.com>
2020-03-03 10:00:44 -0300

    Trabalho 1 - PPW2  03-03-2020

---
+++ panificadora/cabecalho.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Panificadora Cabral</title>
+    <!-- <link href = "./loja/css/bootstrap.css" rel="stylesheet"> -->
+    <link href = "css/bootstrap.css" rel="stylesheet">
+    <link href="css/loja.css" rel="stylesheet">
+    
+</head>
+<body>
+
+    <div class="container">
+        <div class="principal">
+  
---
+++ panificadora/cadastrarProd.php
@@ -0,0 +1,10 @@
+<?php include("cabecalho.php");?>
+    <h1>Cadastrar Produto</h1>
+
+    <nav>
+  <ul class="pager">
+    <li class="previous"><a href="cabecalho.php"><span aria-hidden="true">&larr;</span> </a></li>
+    <li class="next"><a href="cabecalho.php"> <span aria-hidden="true">&rarr;</span></a></li>
+  </ul>
+</nav>
+<?php include("rodape.php");?>
