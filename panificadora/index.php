2a4a33ad032436287fba585c72c4ac6bfc9530de
Marcos314 <marcoswesley314@gmail.com>
2020-03-08 12:11:55 -0300

    update 08-03-2020 12:11

--- panificadora/index.php
+++ panificadora/index.php
@@ -2,98 +2,28 @@
 
 <h1>Padaria <br>Cabral</h1>
 
-<section class="carousel">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
 
-                <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                    <!-- Indicators -->
-                    <ol class="carousel-indicators">
-                        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                        
-                    </ol>
+<main class="main-principal">
 
-                    <!-- Wrapper for slides -->
-                    <div class="carousel-inner" role="listbox">
-                        <div class="item active">
+</main>
 
-                            <div class="row">
+<section>
+    <div class="conteudo">
+        <h2>Um pouco da nossa História..</h2>
+        <p>Bem-vindo a Padaria Cabral
+            Fundada em 30 de Maio de 1994, a Padaria Cabral tem como principais operações a fabricação, venda e distribuição de produtos de panificação, confeitaria e produtos congelados.
+            
+            É uma tradicional empresa com mais de 20 anos de atuação no ramo, reconhecida pela qualidade e variedade dos seus produtos (pães, cucas, bolos, tortas, doces, salgados, produtos congelados, lanches, etc) e serviços, dentre os quais destacam-se o pioneiro e inovador na região “Atendimento 24 Horas”, a distribuição de pães e produtos de confeitaria para empresas (supermercados, casas de lanches, refeitórios industriais, …), o serviço de “Tele Entregas” e a parceria “Ponto Quente”, que consiste na distribuição de pães e salgados congelados em diversas regiões do GO.<br>
+            </p>
 
-                                <div class="col-lg-3 text-left">
-                                    <h2>Padaria Cabral Left1</h2>
-                                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker</p>
-
-                                </div>
-
-                                <!-- <div class="col-lg-3">
-                                    <img src="img/padaria.jpg" class="img-responsive" alt="padaria">
-                                </div>
-
-                                <div class="col-lg-3">
-                                    <img src="img/paodequeijo.jpg" class="img-responsive" alt="paoqueijo">
-                                </div> -->
-
-                                <div class="col-lg-3 text-right">
-                                    <h2>Padaria Cabral Right1</h2>
-                                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker</p>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="item">
-
-                            <div class="row">
-
-                                <div class="col-lg-3 text-left">
-                                    <h2>Padaria Cabral Left2</h2>
-                                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker</p>
-
-                                </div>
-
-                                <!-- <div class="col-lg-3">
-                                    <img src="img/padaria.jpg" class="img-responsive" alt="padaria">
-                                </div>
-
-                                <div class="col-lg-3">
-                                    <img src="img/paodequeijo.jpg" class="img-responsive" alt="paoqueijo">
-                                </div> -->
-
-                                <div class="col-lg-3 text-right">
-                                    <h2>Padaria Cabral Right2</h2>
-                                    <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker</p>
-
-                                </div>
-                            </div>
-                        </div>
-
-                        
-                    </div>
-
-                    <!-- Controls -->
-                    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
-                </div>
-
-            </div>
-
-        </div>
     </div>
 
+
+    <button type="button" class="btn btn-primary btn-lg btn-block">Cadastrar Produtos</button>
 </section>
 
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 
-<script src="js/bootstrap.js"></script>
 
 <?php include("rodape.php"); ?>
